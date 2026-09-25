50722bf197a56724528820872cb03685405601df
Ivan <vaniamar@mail.ru>
2026-09-26 01:32:04 +0700

    feat: add dynamic menu

--- script/menu.js
+++ script/menu.js
@@ -1,3 +1,37 @@
+import data from './products.json' with { type: 'json' };
+console.log(data);
+
+const menuContainer = document.querySelector('.menu-items');
+
+function renderCards(data) {
+    const cardsHTML = data.map(item => {
+        return `
+        
+                        <div class="menu-item">
+                            <div class="item-img">
+                                <img src="${item.image}" alt="${item.name}">
+                            </div>
+                            <div class="menu-content">
+                                <div class="item-title">
+                                    ${item.name}
+                                </div>
+                                <div class="item-text">
+                                ${item.description}
+                                </div>
+                                <div class="item-price">
+                                    $${item.price}
+                                </div>
+                            </div>
+                        </div>
+        `}).join("")
+    return cardsHTML;
+};
+
+menuContainer.innerHTML = renderCards(data);
+
+
+
+
 const categories = document.querySelectorAll('.offer-tab-container');
 categories[0].classList.add('active');
 categories.forEach(category => {
@@ -6,5 +40,14 @@ categories.forEach(category => {
             item.classList.remove('active');
         })
         category.classList.add('active');
+
+        const currentData = category.dataset.category;
+        const filterData = data.filter(item => item.category === currentData);
+        menuContainer.innerHTML = renderCards(filterData);
+        console.log(filterData)
+        console.log(currentData)
     })
 })
+
+
+
